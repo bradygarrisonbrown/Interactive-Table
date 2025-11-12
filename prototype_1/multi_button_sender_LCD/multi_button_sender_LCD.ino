2012f1e14963d485f022ccdf21bbca760f9fbe68
bradygarrisonbrown <113214300+bradygarrisonbrown@users.noreply.github.com>
2025-11-11 21:24:25 -0500

    Working Program
    Sender and receiver allow serial communication

---
+++ prototype_1/multi_button_sender_LCD/multi_button_sender_LCD.ino
@@ -0,0 +1,137 @@
+#include <Wire.h>
+#include <Adafruit_MCP23X17.h>
+
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+//IO Expander
+#include <Adafruit_MCP23X17.h>
+
+//Serial
+#include <SoftwareSerial.h>
+SoftwareSerial link(6, 7);
+
+// NeoPixel setup
+#define LED_PIN     5
+#define LED_COUNT   256
+#define BRIGHTNESS  10
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+int red = 0, green = 0, blue = 0;
+
+Adafruit_MCP23X17 mcp;
+
+//Button Stuff
+#define BLUE_BUTTON  8
+
+#define YELL_BUTTON  9
+
+#define RED_BUTTON  10
+
+#define GREEN_BUTTON  11
+
+#define WHITE_BUTTON  12
+
+
+void setup() {
+  Serial.begin(9600);
+  link.begin(9600);
+  Serial.println("Starting MCP23X17 Button Test...");
+
+  // Initialize MCP23X17 over I2C
+  if (!mcp.begin_I2C()) {
+    Serial.println("MCP23X17 not found! Check wiring or address (default 0x20).");
+    while (1);
+  }
+  Serial.println("MCP23X17 initialized.");
+
+  //Button Setup
+  mcp.pinMode(BLUE_BUTTON, INPUT_PULLDOWN);
+  mcp.pinMode(YELL_BUTTON, INPUT_PULLDOWN);
+  mcp.pinMode(RED_BUTTON, INPUT_PULLDOWN);
+  mcp.pinMode(GREEN_BUTTON, INPUT_PULLDOWN);
+  mcp.pinMode(WHITE_BUTTON, INPUT_PULLDOWN);
+
+  Serial.println("Button pins configured (using internal pull-ups).");
+  Serial.println("Press any button...");
+
+   // Neopixel init
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
+
+  strip.begin();
+  strip.show(); // Turn off all pixels
+  strip.setBrightness(BRIGHTNESS);
+
+  Serial.println("SETUP DONE!");
+}
+
+void loop() {
+  // Buttons are wired to ground, so LOW = pressed
+  byte buttonState = mcp.digitalRead(BLUE_BUTTON);
+  if (buttonState == HIGH) {
+  //Serial.println("BLUE!");
+  // do an action, for example print on Serial
+  buttonUpdateColor(0, 0, 150, "0", "BLUE");
+  }
+  
+
+  buttonState = mcp.digitalRead(YELL_BUTTON);
+  if (buttonState == HIGH) {
+    //Serial.println("YELLOW!");
+    // do an action, for example print on Serial
+    buttonUpdateColor(150, 150, 0, "5000", "YELLOW");
+  }
+  
+
+  buttonState = mcp.digitalRead(RED_BUTTON);
+  if (buttonState == HIGH) {
+    //Serial.println("RED!");
+    // do an action, for example print on Serial
+    buttonUpdateColor(150, 0, 0, "10000", "RED");
+  }
+  
+
+  buttonState = mcp.digitalRead(GREEN_BUTTON);
+  if (buttonState == HIGH) {
+    //Serial.println("GREEN!");
+    // do an action, for example print on Serial
+    buttonUpdateColor(0, 150,  0, "15000", "GREEN");
+  }
+  
+
+  buttonState = mcp.digitalRead(WHITE_BUTTON);
+  
+  if (buttonState == HIGH) {
+    //Serial.println("WHITE!");
+    // do an action, for example print on Serial
+    buttonUpdateColor(150, 150, 150, "20000", "WHITE");
+  }
+
+  
+
+  delay(200); // debounce + avoid spamming the serial monitor
+}
+
+
+void updateLEDs(){
+      // Fill all LEDs with chosen color
+    strip.clear();
+    for (int i = 0; i < LED_COUNT; i++) {
+      strip.setPixelColor(i, strip.Color(red,green,blue));
+    }
+    strip.show();  // Only call once, after filling all pixels
+}
+
+void buttonUpdateColor(int newRed, int newGreen, int newBlue, char* target, char* msg ) {
+  link.println(target);
+  Serial.println(msg);
+  red = newRed;
+  green = newGreen;
+  blue = newBlue;
+  updateLEDs();
+}
--- prototype_1/multi_stepper_reciever/multi_stepper_reciever.ino
+++ prototype_1/multi_stepper_reciever/multi_stepper_reciever.ino
@@ -5,6 +5,13 @@
   #include <avr/power.h>
 #endif
 
+//IO Expander
+#include <Adafruit_MCP23X17.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial link(6, 7);
+
+
 // Stepper setup
 #define HALFSTEP 8
 
@@ -19,27 +26,23 @@
 #define motorBPin3  4
 #define motorBPin4  5
 
-
 AccelStepper stepperA(HALFSTEP, motorAPin1, motorAPin3, motorAPin2, motorAPin4);
 AccelStepper stepperB(HALFSTEP, motorBPin1, motorBPin3, motorBPin2, motorBPin4);
 
 MultiStepper multi;
 
-// NeoPixel setup
-#define LED_PIN     6
-#define LED_COUNT   256
-#define BRIGHTNESS  5
-
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
 // Motion settings
-const float maxSpeed      = 1800.0;
-const float acceleration  = 1000.0;
+const float maxSpeed      = 1000.0;
+const float acceleration  = 500.0;
 
 long targetPosition = 0;
 
+
 void setup() {
-  Serial.begin(115200);
+
+  //Stepper Setup
+  Serial.begin(9600);
+  link.begin(9600);
   Serial.println("AccelStepper Serial Control Ready.");
   Serial.println("Type a target position (e.g. 1000 or -5000).");
 
@@ -51,26 +54,18 @@ void setup() {
   multi.addStepper(stepperA);
   multi.addStepper(stepperB);
   
-
-  // Neopixel init
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-    clock_prescale_set(clock_div_1);
-  #endif
-
-  strip.begin();
-  strip.show(); // Turn off all pixels
-  strip.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
+  //Button Checks
+
   // Check for serial input
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
+  if (link.available() > 0) {
+    String input = link.readStringUntil('\n');
     input.trim();
     if (input.length() > 0) {
       long newPos = input.toInt();
       targetPosition = newPos;
-
       Serial.print("New move command: ");
       Serial.println(targetPosition);
 
@@ -83,23 +78,6 @@ void loop() {
       //multi.runSpeedToPosition(); // Blocks until done
     }
 
-    // --- Set color based on target position ---
-    int red = 0, green = 0, blue = 0;
-
-    if (targetPosition > 5000) {
-      red = 150;
-    } else if (targetPosition >= 3000) {
-      green = 150;
-    } else {
-      blue = 150;
-    }
-
-    // Fill all LEDs with chosen color
-    strip.clear();
-    for (int i = 0; i < LED_COUNT; i++) {
-      strip.setPixelColor(i, strip.Color(red, green, blue));
-    }
-    strip.show();  // Only call once, after filling all pixels
   }
 
   // Continuously drive the stepper
