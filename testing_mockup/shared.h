8264433ff5edf6f5b48ee06fee691f6a0347c235
James Hu <boo15869@gmail.com>
2025-11-20 10:08:36 -0500

    wip testing

---
+++ testing_mockup/shared.h
@@ -0,0 +1,47 @@
+#pragma once
+
+// Dimensions of our grid
+namespace Constants
+{
+    inline constexpr int WIDTH = 3;
+    inline constexpr int HEIGHT = 3;
+}
+
+typedef struct
+{
+    long durationMs;
+    long targets[Constants::HEIGHT][Constants::WIDTH];
+} configuration_t;
+
+inline bool operator==(const configuration_t &lhs, const configuration_t &rhs)
+{
+    if (lhs.durationMs != rhs.durationMs)
+    {
+        return false;
+    }
+
+    for (int y = 0; y < Constants::HEIGHT; ++y)
+    {
+        for (int x = 0; x < Constants::WIDTH; ++x)
+        {
+            if (lhs.targets[y][x] != rhs.targets[y][x])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+typedef struct
+{
+    configuration_t config;
+    long phaseStart;
+} full_state_t;
+
+inline bool operator==(const full_state_t &lhs, const full_state_t &rhs)
+{
+    return lhs.phaseStart == rhs.phaseStart && lhs.config == rhs.config;
+}
+
+full_state_t updateFSM(full_state_t currState, unsigned long clock, bool emergencyStop, bool hasNewConfiguration, configuration_t newestConfiguration);
---
+++ testing_mockup/testing_mockup.ino
@@ -0,0 +1,16 @@
+#include "shared.h"
+
+void setup()
+{
+}
+
+void loop()
+{
+}
+
+full_state_t updateFSM(full_state_t currState, unsigned long clock, bool emergencyStop, bool hasNewConfiguration, configuration_t newestConfiguration)
+{
+    auto ret = currState;
+
+    return ret;
+}
