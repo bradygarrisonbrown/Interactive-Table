ab7c410a7dcec976e75ff3d600b7fb1562a1680c
school <school@schools-MacBook-Air.local>
2025-11-20 16:52:52 -0500

    structure for fsm

--- testing_mockup/testing_mockup.ino
+++ testing_mockup/testing_mockup.ino
@@ -13,9 +13,21 @@ void loop()
 {
 }
 
-full_state_t updateFSM(full_state_t currState, unsigned long clock, bool emergencyStop, bool hasNewConfiguration, configuration_t newestConfiguration)
+full_state_t updateFSM(full_state_t currState, int numRounds, ButtonGrid buttons, unsigned long clock)
 {
+    const auto moleStartMs = currState.moleStartMs;
+    const auto moleDurationMs = currState.moleDurationMs;
+    const auto moleXy = currState.moleXy;
+    const auto currentRound = currState.currentRound;
+    const auto score = currState.score;
+    const auto fsm_state = currState.fsmState;
+
     auto ret = currState;
+
+    switch (fsm_state) {
+    default:
+        Serial.println("Invalid state");
+    }
 
     return ret;
 }
