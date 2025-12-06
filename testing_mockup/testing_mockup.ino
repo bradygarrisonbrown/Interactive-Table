05be08fdd560068f73aba11aea703e4785d4981e
James Hu <boo15869@gmail.com>
2025-12-05 22:08:05 -0500

    [untested] gpio steppers
    basic infrastructure with nasty factory method hack to encapsulate
    gpio stepper knowledge
    
    haven't tested on arduino mega board yet

--- testing_mockup/testing_mockup.ino
+++ testing_mockup/testing_mockup.ino
@@ -2,6 +2,7 @@
 #include "output_util.h"
 #include "button_util.h"
 #include "LED_util.h"
+#include "gpio_stepper_manager.h"
 
 #include <Adafruit_MCP23X17.h>
 
@@ -14,14 +15,16 @@ Adafruit_MCP23X17 mcp1;
 Adafruit_MCP23X17 mcp2;
 Adafruit_MCP23X17 mcp3;
 
-//Emergency Stop Pin
+// Emergency Stop Pin
 constexpr int EMERGENCY_STOP = 3;
 
-//PWM Pin
+// PWM Pin
 constexpr int PWM_PIN = 2;
 
+GpioStepperManager gpioStepperManager = GpioStepperManager::build();
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   while (!Serial)
     ;
@@ -31,43 +34,47 @@ void setup() {
   while (true)
     ;
 #else
-    Serial.println("Testing NOT enabled");
+  Serial.println("Testing NOT enabled");
 
-    initializeLED();
+  initializeLED();
 
-    //PWM LED
-    analogWrite(PWM_PIN, 0);
+  // PWM LED
+  analogWrite(PWM_PIN, 0);
 
-    // Help I2C keep up (I got this from asking LLM, says it should be safe)
-    Wire.setClock(400000);
-    if (!mcp1.begin_I2C()) {
-      Serial.println("Error starting mcp1");
-    }
+  // Help I2C keep up (I got this from asking LLM, says it should be safe)
+  Wire.setClock(400000);
+  if (!mcp1.begin_I2C())
+  {
+    Serial.println("Error starting mcp1");
+  }
 
-    if (!mcp2.begin_I2C(0x20 + 1)) {
-      Serial.println("Error starting mcp2");
-    }
+  if (!mcp2.begin_I2C(0x20 + 1))
+  {
+    Serial.println("Error starting mcp2");
+  }
 
-    if (!mcp3.begin_I2C(0x20 + 2)) {
-      Serial.println("Error starting mcp3");
-    }
-    STEPPER_MANAGER.initialize(&mcp1, &mcp2, &mcp3);
+  if (!mcp3.begin_I2C(0x20 + 2))
+  {
+    Serial.println("Error starting mcp3");
+  }
+  STEPPER_MANAGER.initialize(&mcp1, &mcp2, &mcp3);
+  gpioStepperManager.initialize();
+  initializeButtons();
+  attachInterrupt(digitalPinToInterrupt(EMERGENCY_STOP), ISR, RISING);
 
-
-    initializeButtons();
-    attachInterrupt(digitalPinToInterrupt(EMERGENCY_STOP), ISR, RISING);
-
-    Serial.println("Enter number of rounds:");
+  Serial.println("Enter number of rounds:");
 #endif
 }
 
-  //Emergency stop interrupt function
-void ISR(){
-  //serial.println("EMERGENCY STOP!")
+// Emergency stop interrupt function
+void ISR()
+{
+  // serial.println("EMERGENCY STOP!")
   E_STOP = false;
 }
 
-void loop() {
+void loop()
+{
   // const auto btns = readButtons();
   // for (int y = 0; y < Constants::HEIGHT; ++y) {
   //   for (int x = 0; x < Constants::WIDTH; ++x) {
@@ -79,61 +86,67 @@ void loop() {
   //   }
   // }
 
-    if (Serial.available() > 0 && NUM_ROUNDS == -1) {
-      NUM_ROUNDS = Serial.parseInt();
-      if (NUM_ROUNDS <= 0) {
-        Serial.println("Please enter non-zero rounds.");
-        NUM_ROUNDS = -1;
+  if (Serial.available() > 0 && NUM_ROUNDS == -1)
+  {
+    NUM_ROUNDS = Serial.parseInt();
+    if (NUM_ROUNDS <= 0)
+    {
+      Serial.println("Please enter non-zero rounds.");
+      NUM_ROUNDS = -1;
 
-        Serial.read(); // expect this to be a \n
-      } else {
-        Serial.print("Starting with number of rounds: ");
-        Serial.println(NUM_ROUNDS);
-      }
+      Serial.read(); // expect this to be a \n
     }
-
-    if (NUM_ROUNDS == -1) {
-      // We don't want to do anything until the user says how many rounds
-      fillLEDs(COLOR_BLUE);
-      return;
+    else
+    {
+      Serial.print("Starting with number of rounds: ");
+      Serial.println(NUM_ROUNDS);
     }
+  }
 
-    //If ISR in button_util sets emergency stop to false, enter infinite loop to stop running motors
-    if (!E_STOP){
-      fillLEDs(COLOR_RED);
-      Serial.println("EMERGENCY STOP!");
-      while(true){}
+  if (NUM_ROUNDS == -1)
+  {
+    // We don't want to do anything until the user says how many rounds
+    fillLEDs(COLOR_BLUE);
+    return;
+  }
+
+  // If ISR in button_util sets emergency stop to false, enter infinite loop to stop running motors
+  if (!E_STOP)
+  {
+    fillLEDs(COLOR_RED);
+    Serial.println("EMERGENCY STOP!");
+    while (true)
+    {
     }
+  }
 
-    static full_state_t fs = {
-        /* moleStartMs=*/ 0,
-        /* moleDurationMs=*/ 0,
-        /* moleXy=*/ {-1, -1},
-        /* currentRound=*/ 0,
-        /* totalRounds=*/ 0,
-        /* score=*/ 0,
-        /* fsmState=*/ FsmState::s_INIT,
-    };
+  static full_state_t fs = {
+      /* moleStartMs=*/0,
+      /* moleDurationMs=*/0,
+      /* moleXy=*/{-1, -1},
+      /* currentRound=*/0,
+      /* totalRounds=*/0,
+      /* score=*/0,
+      /* fsmState=*/FsmState::s_INIT,
+  };
 
-//   long dist = stepper[m.x][m.y].distanceToGo();
-    long dist = STEPPER_MANAGER.distanceToGo(fs.moleXy);
+  long dist = gpioStepperManager.distanceToGo(fs.moleXy);
 
-    fs = updateFSM(fs,
-                NUM_ROUNDS,
-                readButtons(),
-                dist,
-                millis());
+  fs = updateFSM(fs,
+                 NUM_ROUNDS,
+                 readButtons(),
+                 dist,
+                 millis());
 
-    delay(10);
-
+  delay(10);
 }
 
 full_state_t updateFSM(full_state_t currState,
                        int numRounds,
                        ButtonGrid buttons,
                        long moleDistanceToGo,
-                       unsigned long clock
-                       ) {
+                       unsigned long clock)
+{
   const auto moleStartMs = currState.moleStartMs;
   const auto moleDurationMs = currState.moleDurationMs;
   const auto moleXy = currState.moleXy;
@@ -144,133 +157,158 @@ full_state_t updateFSM(full_state_t currState,
 
   full_state_t ret = currState;
 
-  //Update PWM pin LED based on score
-  if (score >= 255){
+  // Update PWM pin LED based on score
+  if (score >= 255)
+  {
     analogWrite(PWM_PIN, 255);
-  } else if (score <= 0){
+  }
+  else if (score <= 0)
+  {
     analogWrite(PWM_PIN, 0);
-  } else{
+  }
+  else
+  {
     analogWrite(PWM_PIN, score);
   }
 
-  switch (fsm_state) {
-    // ------------------------------------
-    // INIT → CHOOSE_MOLE
-    // ------------------------------------
-    case FsmState::s_INIT:
-      if (numRounds != 0) {
-        ret.currentRound = 1;
-        ret.totalRounds = numRounds;
-        ret.score = 0;
-        ret.moleDurationMs = 0;
-        ret.moleStartMs = 0;
-        ret.moleXy = {-1, -1};
-        ret.fsmState = FsmState::s_CHOOSE_MOLE;
-        fillLEDs(COLOR_CYAN);
-      }
+  switch (fsm_state)
+  {
+  // ------------------------------------
+  // INIT → CHOOSE_MOLE
+  // ------------------------------------
+  case FsmState::s_INIT:
+    if (numRounds != 0)
+    {
+      ret.currentRound = 1;
+      ret.totalRounds = numRounds;
+      ret.score = 0;
+      ret.moleDurationMs = 0;
+      ret.moleStartMs = 0;
+      ret.moleXy = {-1, -1};
+      ret.fsmState = FsmState::s_CHOOSE_MOLE;
+      fillLEDs(COLOR_CYAN);
+    }
+    break;
+
+  // ------------------------------------
+  // CHOOSE_MOLE → RAISE_MOLE or GAME_OVER
+  // ------------------------------------
+  case FsmState::s_CHOOSE_MOLE:
+    if (currentRound > totalRounds)
+    {
+      // displayLCD("Game Over!", ret.score);
+      Serial.println("Game over!");
+      Serial.print("Score: ");
+      Serial.println(score);
+      ret.fsmState = FsmState::s_GAME_OVER;
       break;
+    }
 
-    // ------------------------------------
-    // CHOOSE_MOLE → RAISE_MOLE or GAME_OVER
-    // ------------------------------------
-    case FsmState::s_CHOOSE_MOLE:
-      if (currentRound > totalRounds) {
-        //displayLCD("Game Over!", ret.score);
-        Serial.println("Game over!");
-        Serial.print("Score: ");
-        Serial.println(score);
-        ret.fsmState = FsmState::s_GAME_OVER;
-        break;
-      }
+    ret.moleXy = randomMole();
+    ret.moleDurationMs = Constants::DEFAULT_MOLE_DURATION;
 
-      ret.moleXy = randomMole();
-      ret.moleDurationMs = Constants::DEFAULT_MOLE_DURATION;
+    // Set the mole's target height
+    // STEPPER_MANAGER.setHeight(ret.moleXy, Constants::MOLE_RISE_HEIGHT);
+    gpioStepperManager.setHeight(ret.moleXy, Constants::MOLE_RISE_HEIGHT);
 
-      // Set the mole's target height
-      STEPPER_MANAGER.setHeight(ret.moleXy, Constants::MOLE_RISE_HEIGHT);
+    setGridColor(moleXy.x, moleXy.y, COLOR_MAGENTA);
 
-      setGridColor(moleXy.x, moleXy.y, COLOR_MAGENTA);
+    ret.fsmState = FsmState::s_RAISE_MOLE;
+    break;
 
-      ret.fsmState = FsmState::s_RAISE_MOLE;
-      break;
+  // ------------------------------------
+  // RAISE_MOLE → WAIT
+  // ------------------------------------
+  case FsmState::s_RAISE_MOLE:
+    // CHECK USING PROVIDED distanceToGoForMole
+    if (moleDistanceToGo == 0)
+    {
+      ret.fsmState = FsmState::s_WAIT;
+      ret.moleStartMs = clock;
+    }
+    else
+    {
+      // STEPPER_MANAGER.step();
+      gpioStepperManager.step();
+    }
+    break;
 
-    // ------------------------------------
-    // RAISE_MOLE → WAIT
-    // ------------------------------------
-    case FsmState::s_RAISE_MOLE:
-      // CHECK USING PROVIDED distanceToGoForMole
-      if (moleDistanceToGo == 0) {
-        ret.fsmState = FsmState::s_WAIT;
-        ret.moleStartMs = clock;
-      } else {
-        STEPPER_MANAGER.step();
-      }
-      break;
+  // ------------------------------------
+  // WAIT → HIT, MISS, TIME_EXPIRED
+  // ------------------------------------
+  case FsmState::s_WAIT:
+  {
+    bool anyPress = false;
+    bool correct = false;
 
-    // ------------------------------------
-    // WAIT → HIT, MISS, TIME_EXPIRED
-    // ------------------------------------
-    case FsmState::s_WAIT:
-      {
-        bool anyPress = false;
-        bool correct = false;
-
-        for (int y = 0; y < 3; y++)
-          for (int x = 0; x < 3; x++)
-            if (buttons[y][x]) {
-              anyPress = true;
-              if (x == moleXy.x && y == moleXy.y) correct = true;
-            }
-
-        if (anyPress && correct) {
-          ret.score = score + 1;
-          ret.fsmState = FsmState::s_HIT_MOLE;
-          setGridColor(moleXy.x, moleXy.y, COLOR_GREEN);
-          Serial.println("HIT");
-        } else if (anyPress && !correct) {
-          ret.score = score - 1;
-          ret.fsmState = FsmState::s_MISS_HIT;
-          setGridColor(moleXy.x, moleXy.y, COLOR_RED);
-          Serial.println("MISS");
-        } else if (clock - moleStartMs > moleDurationMs) {
-          ret.fsmState = FsmState::s_TIME_EXPIRED;
-          setGridColor(moleXy.x, moleXy.y, COLOR_YELLOW);
-          Serial.println("EXPIRED");
+    for (int y = 0; y < 3; y++)
+      for (int x = 0; x < 3; x++)
+        if (buttons[y][x])
+        {
+          anyPress = true;
+          if (x == moleXy.x && y == moleXy.y)
+            correct = true;
         }
-        break;
-      }
 
-    // ------------------------------------
-    // HIT / MISS / TIME_EXPIRED → CLEAR_MOLE
-    // ------------------------------------
-    case FsmState::s_HIT_MOLE:
-    case FsmState::s_MISS_HIT:
-    case FsmState::s_TIME_EXPIRED:
-      STEPPER_MANAGER.setHeight(moleXy, 0);
-      ret.fsmState = FsmState::s_CLEAR_MOLE;
-      break;
+    if (anyPress && correct)
+    {
+      ret.score = score + 1;
+      ret.fsmState = FsmState::s_HIT_MOLE;
+      setGridColor(moleXy.x, moleXy.y, COLOR_GREEN);
+      Serial.println("HIT");
+    }
+    else if (anyPress && !correct)
+    {
+      ret.score = score - 1;
+      ret.fsmState = FsmState::s_MISS_HIT;
+      setGridColor(moleXy.x, moleXy.y, COLOR_RED);
+      Serial.println("MISS");
+    }
+    else if (clock - moleStartMs > moleDurationMs)
+    {
+      ret.fsmState = FsmState::s_TIME_EXPIRED;
+      setGridColor(moleXy.x, moleXy.y, COLOR_YELLOW);
+      Serial.println("EXPIRED");
+    }
+    break;
+  }
 
-    // ------------------------------------
-    // CLEAR_MOLE → CHOOSE_MOLE
-    // ------------------------------------
-    case FsmState::s_CLEAR_MOLE:
-      // Use the provided motor query again
-      if (moleDistanceToGo == 0) {
-        ret.currentRound = currentRound + 1;
-        ret.fsmState = FsmState::s_CHOOSE_MOLE;
-        fillLEDs(COLOR_CYAN);
-      } else {
-        STEPPER_MANAGER.step();
-      }
-      break;
+  // ------------------------------------
+  // HIT / MISS / TIME_EXPIRED → CLEAR_MOLE
+  // ------------------------------------
+  case FsmState::s_HIT_MOLE:
+  case FsmState::s_MISS_HIT:
+  case FsmState::s_TIME_EXPIRED:
+    // STEPPER_MANAGER.setHeight(moleXy, 0);
+    gpioStepperManager.setHeight(moleXy, 0);
+    ret.fsmState = FsmState::s_CLEAR_MOLE;
+    break;
 
-    case FsmState::s_GAME_OVER:
-      fillLEDs(COLOR_BLACK);
-      break;
+  // ------------------------------------
+  // CLEAR_MOLE → CHOOSE_MOLE
+  // ------------------------------------
+  case FsmState::s_CLEAR_MOLE:
+    // Use the provided motor query again
+    if (moleDistanceToGo == 0)
+    {
+      ret.currentRound = currentRound + 1;
+      ret.fsmState = FsmState::s_CHOOSE_MOLE;
+      fillLEDs(COLOR_CYAN);
+    }
+    else
+    {
+      // STEPPER_MANAGER.step();
+      gpioStepperManager.step();
+    }
+    break;
 
-    default:
-      Serial.println("Invalid state");
-      break;
+  case FsmState::s_GAME_OVER:
+    fillLEDs(COLOR_BLACK);
+    break;
+
+  default:
+    Serial.println("Invalid state");
+    break;
   }
 
   return ret;
