2e737cdde9ea4540c11b6ac1f6fa83037b911a98
James Hu <boo15869@gmail.com>
2025-12-07 21:25:28 -0500

    button logic
    mostly porting over old stuff

---
+++ arduino_mega/button_util.ino
@@ -0,0 +1,100 @@
+#include "button_util.h"
+
+//Button Pins
+constexpr int BUTTON_1 = 54;
+constexpr int BUTTON_2 = 55;
+constexpr int BUTTON_3 = 57;
+constexpr int BUTTON_4 = 58;
+constexpr int BUTTON_5 = 60;
+constexpr int BUTTON_6 = 61;
+constexpr int BUTTON_7 = 62;
+constexpr int BUTTON_8 = 63;
+constexpr int BUTTON_9 = 65;
+
+// Global variables to track button state for reliable press detection
+static byte last1   = LOW;
+static byte last2 = LOW;
+static byte last3   = LOW;
+static byte last4  = LOW;
+static byte last5  = LOW;
+static byte last6  = LOW;
+static byte last7  = LOW;
+static byte last8  = LOW;
+static byte last9  = LOW;
+
+//Emergency Stop Pin
+constexpr int EMERGENCY_STOP = 69;
+
+/*
+BUTTON HANDLING CODE
+*/
+
+//Emergency stop interrupt function
+void emergencyStopISR(){
+  //serial.println("EMERGENCY STOP!")
+  while(true){ }
+}
+
+//Initialize button + emergency stop pin settings
+void initializeButtons() {
+  //Button Setup
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  pinMode(BUTTON_3, INPUT_PULLUP);
+  pinMode(BUTTON_4, INPUT_PULLUP);
+  pinMode(BUTTON_5, INPUT_PULLUP);
+  pinMode(BUTTON_6, INPUT_PULLUP);
+  pinMode(BUTTON_7, INPUT_PULLUP);
+  pinMode(BUTTON_8, INPUT_PULLUP);
+  pinMode(BUTTON_9, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(EMERGENCY_STOP), emergencyStopISR, RISING);
+}
+
+//Arrays to keep track of button/Last items
+int buttonIds[9] = {
+    BUTTON_1, BUTTON_2, BUTTON_3,
+    BUTTON_4, BUTTON_5, BUTTON_6,
+    BUTTON_7, BUTTON_8, BUTTON_9
+};
+
+byte* lasts[9] = {
+    &last1, &last2, &last3,
+    &last4, &last5, &last6,
+    &last7, &last8, &last9
+};
+
+//Runs check button on each button and returns 3x3 array of boolean button status
+ButtonGrid readButtons() {
+  ButtonGrid grid;
+
+  for (int i = 0; i < 9; i++) {
+      bool val = checkButton(buttonIds[i], lasts[i]);
+      grid.buttons[i / 3][i % 3] = val;
+  }
+
+  return grid;
+}
+
+//Returns boolean that is true if button successfully pressed
+bool checkButton(uint8_t pin, byte *lastState) {
+  bool ret = false;
+  byte currentState = digitalRead(pin);
+  
+  // If the state has changed since the last loop iteration
+  if (currentState != *lastState) {
+      // Wait for a short debounce period to ensure stability
+      delay(20);
+      
+      // Read the state again after debouncing
+      byte debouncedState = digitalRead(pin);
+
+      if (debouncedState == HIGH) {
+        ret = true;
+      } 
+
+    // Update the last state for the next loop iteration
+    *lastState = currentState;
+  }
+  return ret;
+}
