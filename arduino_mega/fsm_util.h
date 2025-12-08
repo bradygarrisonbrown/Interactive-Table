3c7dcaa24fda44b7a829cb62eab2e00408dd665b
James Hu <boo15869@gmail.com>
2025-12-07 19:29:57 -0500

    migrate tests to arduino mega
    very few changes required, removed some cruft

---
+++ arduino_mega/fsm_util.h
@@ -0,0 +1,16 @@
+#ifndef FSM_UTIL_H
+#define FSM_UTIL_H
+
+#include "shared.h"
+
+#ifndef TESTING
+xy_t randomMole() {
+  return { random(0, 3), random(0, 3) };
+}
+#else
+xy_t randomMole() {
+  return { 1, 1 };
+}
+#endif
+
+#endif
